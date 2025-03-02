747aff7f858dee0b547c571e6125d1572f140b30
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-03-02 19:15:14 +0530

    cors config

--- Backend-Voice-assistant/index.js
+++ Backend-Voice-assistant/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://localhost:5173','https://hackathon-project-sage.vercel.app'],
+    origin: ['http://localhost:5173','https://hackathon-project-sage.vercel.app','https://hackathon-project-restro.vercel.app'],
     credentials: true,
 }));
 
